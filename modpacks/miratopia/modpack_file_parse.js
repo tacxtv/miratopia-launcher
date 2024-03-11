87246d34db58604ff8b0b41f9f85ff88eaa21ee5
Tacx <12997062+tacxou@users.noreply.github.com>
2024-03-11 18:29:25 +0100

    files.json implementation

---
+++ modpacks/miratopia/modpack_file_parse.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+const baseUrl = 'https://raw.githubusercontent.com/tacxtv/miratopia-launcher/config/modpacks/miratopia/modpack/';
+
+function listFilesRecursive(directory) {
+    const files = fs.readdirSync(directory);
+    let fileList = [];
+
+    files.forEach((file) => {
+        const filePath = `${directory}/${file}`;
+        const stats = fs.statSync(filePath);
+
+        if (stats.isDirectory()) {
+            const subdirectoryFiles = listFilesRecursive(filePath);
+            fileList = fileList.concat(subdirectoryFiles);
+        } else {
+            const fileData = fs.readFileSync(filePath);
+            const hash = crypto.createHash('sha1').update(fileData).digest('hex');
+            fileList.push({
+                path: filePath,
+                size: stats.size,
+                hash,
+                url: `${baseUrl}${filePath.replace(`${modpackDirectory}/`, '')}`,
+            });
+        }
+    });
+
+    return fileList;
+}
+
+const modpackDirectory = './modpack';
+const files = listFilesRecursive(modpackDirectory).map((file) => {
+    file.path = file.path.replace(`${modpackDirectory}/`, '');
+    return file;
+});
+
+fs.writeFileSync('files.json', JSON.stringify(files, null, 2));
